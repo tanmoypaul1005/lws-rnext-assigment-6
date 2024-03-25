923f3fc18ebd99db02e63f68bbb260c1634f629f
tanmoypaul007 <tanmoypaul1005@gmail.com>
2024-03-25 17:58:02 +0600

    Add header section to Home page

--- app/page.js
+++ app/page.js
@@ -8,7 +8,6 @@ export default function Home() {
         <nav class="flex items-center justify-between w-11/12 lg:w-10/12 max-w-7xl py-5 lg:py-6 mx-auto">
           <div class="flex gap-5 items-center justify-between lg:w-8/12 text-[#1A1A1A]">
             <div class="flex items-center gap-2">
-
               <Image
                 src={'/assets/svg/menu.svg'}
                 alt="menu"
@@ -32,6 +31,20 @@ export default function Home() {
             <Image src={'/assets/svg/shopping-Cart.svg'} width={20} height={20} className="block w-5 h-5" alt="shopping cart icon" />
           </div>
         </nav>
+
+        <header
+          className="h-[500px] bg-center flex flex-col-reverse bg-cover w-full"
+          style={{ backgroundImage: "url('/assets/header.webp')" }}
+        >
+          <div class="mb-5 w-10/12 max-w-7xl text-white mx-auto">
+            <h1 class="text-2xl lg:text-3xl font-serif">Introducing LWS Shop Center</h1>
+            <p>Your whole week at a glance</p>
+            <button
+              class="bg-[#ffd160] hover:bg-[#e4be60] border border-black w-60 mt-2 py-3 rounded-full text-center text-black">
+              Get the Weekly Kit
+            </button>
+          </div>
+        </header>
       </body>
 
     </>
