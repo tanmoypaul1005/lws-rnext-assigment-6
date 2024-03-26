75c725c609b7fc5fc74eca01b20a6d8cc5ec9ec4
tanmoypaul007 <tanmoypaul1005@gmail.com>
2024-03-26 09:49:14 +0600

    Refactor product list rendering in Category component

--- app/category/[categoryName]/page.js
+++ app/category/[categoryName]/page.js
@@ -10,7 +10,6 @@ const Category = ({ params }) => {
     return (
         <div>
             <main>
-
                 <section className="items-start justify-between w-11/12 py-0 mx-auto lg:w-10/12 max-w-7xl lg:py-10 lg:flex">
                     <div className="flex items-center justify-between w-full my-10 lg:block lg:w-2/12 lg:my-0 lg:mt-4">
                         <button className="box-border block h-6 mt-4 border-black hover:border-b">All</button>
@@ -22,21 +21,25 @@ const Category = ({ params }) => {
                     </div>
                     <div className="sticky top-0 right-0 grid w-full grid-cols-2 gap-4 my-4 lg:w-10/12 lg:grid-cols-3 lg:my-10">
 
-                        <div>
+                        {
+                            product_list?.map((product, index) => (
+                                <div key={index}>
+                                    <Image width={180} height={270} src={product?.thumbnail} alt="" classNameName="relative delay-150 w-180px lg:w-full h-[205px] lg:h-[310px] bg-[#f8f8f8]  bg-cover bg-center transition-all duration-3000 ease-in-out transform" />
+                                    <h2 className="mt-2 text-sm lg:text-base">
+                                        <a className="text-base font-bold" href="./productPage.html">
+                                            {product?.title}
+                                        </a>
+                                        <span className="text-[#919090]">
+                                            <a href="./category.html">(Smartphones)</a>
+                                        </span>
+                                    </h2>
+                                    <p className="text-[#919090] text-sm ">{product?.description}</p>
 
-                            <Image width={180} height={270} src={"/assets/products/iphone.jpg"} alt="" classNameName="relative delay-150 w-180px lg:w-full h-[205px] lg:h-[310px] bg-[#f8f8f8]  bg-cover bg-center transition-all duration-3000 ease-in-out transform" />
-                            <h2 className="mt-2 text-sm lg:text-base">
-                                <a className="text-base font-bold" href="./productPage.html">
-                                    iPhone 9
-                                </a>
-                                <span className="text-[#919090]">
-                                    <a href="./category.html">(Smartphones)</a>
-                                </span>
-                            </h2>
-                            <p className="text-[#919090] text-sm ">An apple mobile which is nothing like apple</p>
+                                    <p className="mt-4 text-sm text-rose-600"><span className="text-[#919090] line-through">$205.00</span> $195.00</p>
+                                </div>
+                            ))
+                        }
 
-                            <p className="mt-4 text-sm text-rose-600"><span className="text-[#919090] line-through">$205.00</span> $195.00</p>
-                        </div>
 
 
 
